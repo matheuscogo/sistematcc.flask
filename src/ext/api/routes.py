473d5246a9a51e6f5e6901e24e3bfe725bd08fba
Matheus Cogo <matheus.2018011821@aluno.iffar.edu.br>
2021-10-25 20:28:28 -0300

    Descrição:Teste de rota

--- src/ext/api/routes.py
+++ src/ext/api/routes.py
@@ -1,22 +1,12 @@
 from flask import render_template, request, Blueprint, redirect
-import json
+from datetime import datetime
 
 bp_controller = Blueprint('route', __name__)
 
 
 @bp_controller.route('/api/cadastrarMatriz', methods=['POST', 'GET'])
 def cadastrarMatriz():  # Cadastrar Matriz
-    try:
-        templateData = {
-            'title': 'Sistema de gerenciamento de matrizes',
-            'overview': 'Matrizes',
-            'color': 'green',
-        }
-        return render_template("index.html", **templateData)
+    try:  
+        return {'time': datetime.now().strftime("%H:%M:%S")}
     except BaseException as e:
-        templateData = {
-            'title': 'Sistema de gerenciamento de matrizes',
-            'color': 'green',
-            'aviso': "Erro ao cadastrar Matriz"
-        }
-        return "isso é o cadastra matriz teste"
+        return {'time': datetime.now().strftime("%H:%M:%S")}
